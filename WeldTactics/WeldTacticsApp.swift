a4acea57cc64dec9ceea5b623a4b92dcae8ed60f
Joseph DeWeese <inclinedtactics@icloud.com>
2024-04-17 18:50:25 -0500

    Initial Commit

---
+++ WeldTactics/WeldTacticsApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeldTacticsApp.swift
+//  WeldTactics
+//
+//  Created by Joseph DeWeese on 4/17/24.
+//
+
+import SwiftUI
+
+@main
+struct WeldTacticsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
